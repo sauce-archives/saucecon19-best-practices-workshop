7922b2e57db13de5471030536f588d1e1e37babe
James Tacker <james.tacker@saucelabs.com>
2019-02-21 16:38:18 -0800

    02_create_page_objects

---
+++ src/test/java/exercises/LogInTest.java
@@ -0,0 +1,7 @@
+package exercises;
+
+import org.openqa.selenium.WebDriver;
+
+public class LogInTest  {
+    protected WebDriver driver;
+}
---
+++ src/test/java/pages/LogInPage.java
@@ -0,0 +1,7 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class LogInPage {
+    WebDriver driver;
+}
