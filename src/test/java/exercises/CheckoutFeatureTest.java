88510a16da05ea1ed72d31cb80a60158f7404fba
James Tacker <james.tacker@saucelabs.com>
2019-03-08 10:01:07 -0800

    Changed page and test object code to reflect new workshop flow

--- src/test/java/exercises/CheckoutFeatureTest.java
+++ src/test/java/exercises/CheckoutFeatureTest.java
@@ -1,101 +1,19 @@
 package exercises;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import pages.LoginPage;
-
+import pages.CheckoutPage;
 import java.util.concurrent.TimeUnit;
 
-public class CheckoutFeatureTest extends BaseTest{
+public class CheckoutFeatureTest extends BaseTest {
     @Test
     public void ShouldBeAbleToCheckoutWithItems() {
-
-        // Specify Data
-        String firstname = "john";
-        String lastname = "doe";
-        String postal = "94040";
-
-        String backpack = "div:nth-child(1) > div.pricebar > button";
-        String jacket = "div:nth-child(4) > div.pricebar > button";
-        String cart = "#shopping_cart_container";
-        String rmvBtn = "div:nth-child(4) > div.cart_item_label > div.item_pricebar > button";
-        String continueShopping = "a.cart_cancel_link";
-        String checkoutLink = "a.cart_checkout_link";
-        String firstNameField = "[data-test='firstName']";
-        String lastNameField = "[data-test='lastName']";
-        String postalField= "[data-test='postalCode']";
-        String cartCheckout = "[value='CONTINUE']";
-        String finished = "a.cart_checkout_link";
-        String complete = "https://www.saucedemo.com/checkout-complete.html";
-
         // wait 5 seconds
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS) ;
-        //navigate to the url of the Sauce Labs Sample app
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.visit();
-
-        // Ignore the following selectors
-        String username = "standard_user";
-        String password = "secret_sauce";
-        loginPage.login(username, password);
-
-        // Assert that the url is on the inventory page
-        Assert.assertEquals("https://www.saucedemo.com/inventory.html", driver.getCurrentUrl());
-
-        driver.findElement(By.cssSelector(backpack)).click();
-        driver.findElement(By.cssSelector(jacket)).click();
-
-        // proceed to checkout
-        driver.findElement(By.cssSelector(cart)).click();
-
-        // wait 5 seconds
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS) ;
-
-        // remove item from cart
-        driver.findElement(By.cssSelector(rmvBtn)).click();
-
-        // continue shopping
-        driver.findElement(By.cssSelector(continueShopping)).click();
-
-        // wait 5 seconds
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS) ;
-
-        // re-add item to cart and proceed to checkout
-        driver.findElement(By.cssSelector(jacket)).click();
-        driver.findElement(By.cssSelector(cart)).click();
-
-        // wait 5 seconds
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-        //Click Checkout Link
-        driver.findElement(By.cssSelector(checkoutLink)).click();
-
-        // wait 5 seconds
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-        // proceed to shipping info (checkout step 1)
-        driver.findElement(By.cssSelector(firstNameField)).sendKeys(firstname);
-        driver.findElement(By.cssSelector(lastNameField)).sendKeys(lastname);
-        driver.findElement(By.cssSelector(postalField)).sendKeys(postal);
-
-        // wait 5 seconds
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-        //Click Cart Checkout Link
-        driver.findElement(By.cssSelector(cartCheckout)).click();
-
-        // wait 5 seconds
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-        //  proceed to confirmation page (checkout step 2)
-        driver.findElement(By.cssSelector(finished)).click();
-
-        // wait 5 seconds
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-        // assert that the test is finished by checking the last page's URL
-        Assert.assertEquals(driver.getCurrentUrl(), complete);
+        CheckoutPage checkoutPage = new CheckoutPage(driver);
+        checkoutPage.visit();
+        checkoutPage.setCartState();
+        checkoutPage.checkout();
+        Assert.assertTrue(checkoutPage.hasItems());
     }
 }
--- src/test/java/pages/CheckoutPage.java
+++ src/test/java/pages/CheckoutPage.java
@@ -1,11 +1,34 @@
 package pages;
 
 import exercises.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 public class CheckoutPage extends BaseTest {
 
     public CheckoutPage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public void visit() {
+        driver.get("https://www.saucedemo.com/cart.html");
+    }
+
+    public void setCartState() {
+        driver.navigate().refresh();
+        ((JavascriptExecutor)driver).executeScript("window .sessionStorage .setItem('standard-username', 'standard-user')");
+        ((JavascriptExecutor)driver).executeScript("window .sessionStorage .setItem('cart-contents', '[4,1]')");
+        driver.navigate().refresh();
+    }
+
+    public void checkout() {
+        String checkoutLink = "cart_checkout_link";
+        driver.findElement(By.className(checkoutLink)).click();
+    }
+
+    public Boolean hasItems() {
+        String cartBadge = "shopping_cart_badge";
+        return Integer.parseInt(driver.findElement(By.className(cartBadge)).getText()) > 0;
     }
 }
