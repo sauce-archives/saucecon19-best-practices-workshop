0a9f7bb7c4b541759bf25ad18d37bed55fcc299a
James Tacker <james.tacker@saucelabs.com>
2019-03-11 09:25:28 -0700

    Update exercise guides

--- src/test/java/exercises/CheckoutFeatureTest.java
+++ src/test/java/exercises/CheckoutFeatureTest.java
@@ -1,19 +1,30 @@
 package exercises;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.CheckoutPage;
+import pages.CheckoutCompletePage;
+import pages.ConfirmationPage;
+
 import java.util.concurrent.TimeUnit;
 
-public class CheckoutFeatureTest extends BaseTest {
+
+public class CheckoutFeatureTest extends BaseTest{
+
     @Test
     public void ShouldBeAbleToCheckoutWithItems() {
-        // wait 5 seconds
+
+        // TODO deal with this in the future: wait 5 seconds
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS) ;
-        CheckoutPage checkoutPage = new CheckoutPage(driver);
-        checkoutPage.visit();
-        checkoutPage.setCartState();
-        checkoutPage.checkout();
-        Assert.assertTrue(checkoutPage.hasItems());
+
+        ConfirmationPage confirmationPage = new ConfirmationPage(driver);
+        confirmationPage.visit();
+        confirmationPage.setPageState();
+        Assert.assertTrue(confirmationPage.hasItems());
+
+        CheckoutCompletePage completePage = confirmationPage.FinishCheckout();
+        // assert that the test is finished by checking the last page's URL
+        Assert.assertTrue(completePage.IsLoaded());
     }
+
 }
