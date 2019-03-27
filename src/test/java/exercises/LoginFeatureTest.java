386a6afab39b299d022948dca70255f2aa01f3a7
nikolay.advolodkin <nikolay.advolodkin@saucelabs.com>
2019-03-27 13:17:24 -0400

    Removed redundant comments

--- src/test/java/exercises/LoginFeatureTest.java
+++ src/test/java/exercises/LoginFeatureTest.java
@@ -10,14 +10,10 @@ public class LoginFeatureTest extends BaseTest  {
 
     @Test
     public void ShouldBeAbleToLogin() {
-
-        // wait 5 seconds
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS) ;
-        //navigate to the url of the Sauce Labs Sample app
         LoginPage loginPage = new LoginPage(driver);
         loginPage.visit();
 
-        // Ignore the following selectors
         String username = "standard_user";
         String password = "secret_sauce";
         loginPage.login(username, password);
