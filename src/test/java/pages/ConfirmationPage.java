32d73a64c290ac4e116edbf670e5ef6e09aa5f9f
James Tacker <james.tacker@saucelabs.com>
2019-04-05 09:55:48 -0700

    optimized imports

--- src/test/java/pages/ConfirmationPage.java
+++ src/test/java/pages/ConfirmationPage.java
@@ -4,12 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import javax.xml.bind.Element;
 
 public class ConfirmationPage extends BasePage {
     public ConfirmationPage(WebDriver driver) {
