b1185677bdb761fa60c7213df5ddfd64532fbecd
nikolay.advolodkin <nikolay.advolodkin@saucelabs.com>
2019-04-17 15:52:19 -0400

    minor clean up

--- src/test/java/exercises/CheckoutFeatureTest.java
+++ src/test/java/exercises/CheckoutFeatureTest.java
@@ -50,15 +50,10 @@ public class CheckoutFeatureTest {
     public void ShouldBeAbleToCheckoutWithItems() {
         // wait 5 seconds
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS) ;
-        //navigate to the url of the Sauce Labs Sample app
         LoginPage loginPage = new LoginPage(driver);
         loginPage.visit();
 
-        // Ignore the following selectors
-        String username = "standard_user";
-        String password = "secret_sauce";
-        InventoryPage inventoryPage = loginPage.login(username, password);
-
+        InventoryPage inventoryPage = loginPage.login("standard_user", "secret_sauce");
         // Assert that the url is on the inventory page
         //TODO fix this assertion later
         Assert.assertEquals("https://www.saucedemo.com/inventory.html", driver.getCurrentUrl());
