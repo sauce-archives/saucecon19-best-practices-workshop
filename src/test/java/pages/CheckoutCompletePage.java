e8ee1c1156ad1995e25d39cb263a5ce985ac1359
nikolay.advolodkin <nikolay.advolodkin@saucelabs.com>
2019-04-18 15:38:05 -0400

    update method name

--- src/test/java/pages/CheckoutCompletePage.java
+++ src/test/java/pages/CheckoutCompletePage.java
@@ -2,7 +2,6 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CheckoutCompletePage extends BasePage {
 
