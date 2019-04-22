6c7019ed8742b9c8fb917a300191d8d3189021fa
nikolay.advolodkin <nikolay.advolodkin@saucelabs.com>
2019-04-22 15:32:05 -0500

    fixed bug with LoginPage inheriting from BaseTest

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -4,7 +4,9 @@ import exercises.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class LoginPage extends BaseTest {
+public class LoginPage {
+
+    private final WebDriver driver;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
